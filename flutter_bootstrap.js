12a6650582e1685952ada7fc88183ce560dd88dd
shivprasadphadatare <shivphadatare214@gmail.com>
2025-04-10 23:43:11 +0530

    first commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "404630056"
+    serviceWorkerVersion: "1467912338"
   }
 });
